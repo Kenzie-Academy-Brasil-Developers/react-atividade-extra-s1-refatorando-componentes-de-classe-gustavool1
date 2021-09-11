eccfc0c6b7a029d1ed708663277ac6b795dc80e8
gustavo <go8895806@gmail.com>
2021-09-11 19:53:59 -0300

    .

--- src/components/SearchWeather/index.js
+++ src/components/SearchWeather/index.js
@@ -1,43 +1,35 @@
-import { Component } from "react";
-
+import {  useEffect } from "react";
+import { useState } from "react/cjs/react.development";
 import "./style.css";
+const SearchWeather = ({handleChangeWeather}) =>{
+  const [inputValue, setInputValue ] = useState('Curitiba')
 
-class SearchWeather extends Component {
-  state = {
-    inputValue: "Curitiba",
-  };
+  useEffect(()=>{
+    handleSubmit();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
 
-  componentDidMount() {
-    this.handleSubmit();
-  }
-
-  handleSubmit = () => {
-    const { inputValue } = this.state;
-    const { handleChangeWeather } = this.props;
-
-    fetch(`https://goweather.herokuapp.com/weather/${this.state.inputValue}`)
+  const handleSubmit = () => {
+    fetch(`http://goweather.herokuapp.com/weather/${inputValue}`)
       .then((response) => response.json())
       .then((response) => handleChangeWeather(response, inputValue));
   };
-
-  render() {
     return (
       <div className="search-container">
         <div>
           <input
             className="search-input"
-            value={this.state.inputValue}
-            onChange={(e) => this.setState({ inputValue: e.target.value })}
+            value={inputValue}
+            onChange={((event)=>setInputValue(event.target.value))}
           />
         </div>
         <div>
-          <button className="search-button" onClick={this.handleSubmit}>
+          <button className="search-button" onClick={handleSubmit}>
             Search
           </button>
         </div>
       </div>
     );
-  }
 }
 
 export default SearchWeather;
